05428ddea4ea21f8d7d7fc79afd25b40561de855
valentinbdv <valentin.pichard@bondevisite.fr>
2018-02-02 16:21:59 +0100

    update path for images

--- merge.js
+++ merge.js
@@ -1,5 +1,6 @@
 var jsonfile = require('jsonfile')
 var fs = require('fs');
+var path = require('path');
 var copy = require('copy');
 
 var dirname = './animations/';
@@ -11,6 +12,7 @@ mergeLottieJson(dirname, outputjson);
 
 function mergeLottieJson (dirname, output, callback) {
   var bigjson = {};
+  emptyFolder(outputimages);
   fs.readdir(dirname, function(err, list) {
     dirname = fs.realpathSync(dirname);
     if (err) return console.error(err);
@@ -19,7 +21,7 @@ function mergeLottieJson (dirname, output, callback) {
     list.forEach(function(folder) {
       var folderpath = dirname + '/' + folder;
       var json = folderpath + '/data.json';
-      var images = folderpath+'/images/';
+      var images = folderpath + '/images/';
       jsonfile.readFile(json, function(err, obj) {
         if (err) return console.error(err);
         bigjson[folder] = obj;
@@ -32,7 +34,6 @@ function mergeLottieJson (dirname, output, callback) {
 }
 
 function copyImagesAnim (path) {
-  console.log(path+'*.png');
   copy(path+'*.png', outputimages, function(err, files) {
     if (err) return console.error(err);
   });
@@ -41,5 +42,26 @@ function copyImagesAnim (path) {
 function createOutput (bigjson, output) {
   jsonfile.writeFile(output, bigjson, function (err) {
     if (err) return console.error(err);
+    //// REPLACE IMAGE LINKS
+    fs.readFile(output, 'utf8', function (err, data) {
+      if (err) return console.log(err);
+      var result = data.replace(/images\//g, 'https:\/\/asset.bondevisite.fr\/animation\/mergelotties\/images\/');
+
+      fs.writeFile(output, result, 'utf8', function (err) {
+         if (err) return console.log(err);
+      });
+    });
+  });
+}
+
+function emptyFolder (directory) {
+  fs.readdir(directory, (err, files) => {
+    if (err) throw err;
+
+    for (const file of files) {
+      fs.unlink(path.join(directory, file), err => {
+        if (err) throw err;
+      });
+    }
   });
 }
